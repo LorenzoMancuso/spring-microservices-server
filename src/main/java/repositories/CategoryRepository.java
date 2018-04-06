9753dc4b640893066c4a2af1d633fe1e43edbfdb
Luca Laudiero <luca150695@gmail.com>
2018-04-06 16:11:51 +0200

    Added findOne in CategoryRepository

--- src/main/java/repositories/CategoryRepository.java
+++ src/main/java/repositories/CategoryRepository.java
@@ -14,5 +14,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends CrudRepository<entities.Category, Long> {
-    
+    entities.Category findOne(Long idCategory);
 }
--- src/main/java/services/CategoryController.java
+++ src/main/java/services/CategoryController.java
@@ -5,13 +5,12 @@
  */
 package services;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
+import entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repositories.CategoryRepository;
 
 
 /**
@@ -21,8 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    CategoryRepository categoryRepository;
     
-    //1) Tutte le categorie
+    @RequestMapping(/*method = GET,*/ value = "/findall")
+    public String findAll(){
+        String result = "";
+
+        System.out.println(categoryRepository.findAll());
+        
+        for(Category cust : categoryRepository.findAll()){
+            result += cust + "<br>";
+        }
+        return result;
+    }
+    
+    @RequestMapping(/*method = GET,*/ value = "/get-category/{idCategory}")
+    public Category getCategory(@PathVariable Long idCategory){
+        Category result = categoryRepository.findOne(idCategory);
+        System.out.println(result.toString());
+        return result;
+    }
+    
+    //FATTO 1) Tutte le categorie
     //2) ricerca di una categoria per id
     //3) lista delle categorie più popolari (collegamento a interest)
     
